c08002a40544fd89b3da01a5644265ac2d184de8
Geyumi <120091023+geyumi@users.noreply.github.com>
2023-10-17 11:50:37 +0530

    write page done

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
 <div>
 <TopBar/>
-<Single/>
+<Write/>
 </div>
 
   );
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -22,7 +22,7 @@ export default function SinglePost() {
             Author:
             <b className="singlePostAuthor">
               
-                Safak
+                Geyumi
             
             </b>
           </span>
