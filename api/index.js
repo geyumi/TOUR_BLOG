dec62e26291cc1a861ff9d61f02540e0e6bfa647
Geyumi <120091023+geyumi@users.noreply.github.com>
2023-10-17 07:43:23 +0530

    third commit

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ const express= require('express');
 const app=express();
 const dotenv= require("dotenv");
 const mongoose = require("mongoose");
+const multer= require("multer");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const catogoryRoute = require("./routes/catogories");
 dotenv.config();
 
 app.use(express.json());
@@ -14,13 +16,32 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   
-  }).then(console.log("Connected to MongoDB"))
-  .catch((error)=>console.log("Error connecting to MongoDB"))
+  }).then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.log("Error connecting to MongoDB"));
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,"hello.jpg")
+    } 
+});
+
+
+
+const upload= multer({storage:storage});
+
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+res.status(200).json("File has been uploaded")});
 
 
  app.use("/api/auth",authRoute);
  app.use("/api/users",userRoute);
  app.use("/api/posts",postRoute);
+ app.use("/api/catogories",catogoryRoute);
 
  app.listen("5000", () => {
    
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -13,7 +13,7 @@ router.post("/",  async (req, res) => {
   }
 });
 
-//UPDATE POST
+//UPDATE POST===================
 router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
